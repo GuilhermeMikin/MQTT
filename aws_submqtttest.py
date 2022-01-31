28001aac729236707b0c14190f34ade79d1a367c
MiKiNGuilherme <guilhermebalopes@ufu.br>
2022-01-31 10:28:44 -0300

    updt

--- aws_submqtttest.py
+++ aws_submqtttest.py
@@ -1,7 +1,6 @@
 import time as t
 import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
 
-# Define ENDPOINT, CLIENT_ID, PATH_TO_CERTIFICATE, PATH_TO_PRIVATE_KEY, PATH_TO_AMAZON_ROOT_CA_1, MESSAGE, TOPIC, and RANGE
 ENDPOINT = "ax7j1tokadxlb-ats.iot.us-east-2.amazonaws.com"
 CLIENT_ID = "testDevice"
 PATH_TO_CERTIFICATE = "D:\\AWS\\thingtest\\31d8a33563-certificate.pem.crt"
